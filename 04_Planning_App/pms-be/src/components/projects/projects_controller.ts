db4dea0ff69ff232270cff0a62a24e2312e27b57
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-07-04 10:47:20 -0300

    Project Routes e Controller

---
+++ 04_Planning_App/pms-be/src/components/projects/projects_controller.ts
@@ -0,0 +1,17 @@
+export class ProjectController {
+  public addHandler() {
+    // Logic to add a project
+  }
+  public getAllHandler() {
+    // Logic to get all projects
+  }
+  public getDetailsHandler() {
+    // Logic to get project details
+  }
+  public async updateHandler() {
+    // Logic to update a project
+  }
+  public async deleteHandler() {
+    // Logic to delete a project
+  }
+}
